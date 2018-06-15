9f35767e9203d7130e7ee7db60d5014e98bc8a52
SONG Mozi (CM/ESW-CN) <smo1szh@bosch.com>
2018-06-15 17:24:27 +0800

    add alert

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
-var alerts = ["啊！你戳我！"， "这位飞鱼，您好"， "你谁啊？", "晚饭撸串？", "下班了？", "这里只有两条飞鱼，其他都是咸鱼，咸鱼仔，咸鱼干", "hohoho", "hahaha"]
+var alerts = ["啊！你戳我！", "这位飞鱼，您好", "你谁啊？", "晚饭撸串？", "下班了？", "这里只有两条飞鱼，其他都是咸鱼，咸鱼仔，咸鱼干", "hohoho", "hahaha"];
 
-document.querySelector('button').click = function(){
-	var random = getRandomArbitrary(0, 7)
-	alert("jibu");
+function clicked(){
+	var index = getRandomArbitrary(0,7);
+	document.getElementById("demo").innerHTML = alerts[index];
 }
-alert("jibu");
+
 function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
