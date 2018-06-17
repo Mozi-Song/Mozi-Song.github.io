fce55c0ef3e1e9ae853ae0c269fb27a8b52881d6
mozi <mozi.song@outlook.com>
2018-06-17 18:55:52 +0800

    update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
-var alerts = ["啊！你戳我！", "这位飞鱼，您好", "你谁啊？", "晚饭撸串？", "下班了？", "这里只有两条飞鱼，其他都是咸鱼，咸鱼仔，咸鱼干", "hohoho", "hahaha"];
+var alerts = ["啊！你戳我！", "这位飞鱼，您好", "你谁啊？", "晚饭撸串？", "下班了？", "这里只有两条飞鱼，其他都是咸鱼，咸鱼仔，咸鱼干", "hohoho", "hahaha", "科技始终来源于人性，Ken Thompson写出Unix内核是因为他想玩\"太空旅游\"", "优秀的人做一件事，即使需要排山倒海也会做到", "开源的理念激励人们更好地写出自己的代码，且能得到其他人的指点"];
 
 function clicked(){
-	var index = getRandomArbitrary(0,7);
+	var index = getRandomArbitrary(0,alerts.length-1);
 	document.getElementById("demo").innerHTML = alerts[index];
 }
 
